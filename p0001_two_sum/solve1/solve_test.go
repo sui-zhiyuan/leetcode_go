59cc16438d7dc758b991b1bfc9dadaca76538ab1
Zhiyuan Sui <zhiyuan.sui@grabtaxi.com>
2019-02-04 19:22:37 +0800

    fix p0001

---
+++ p0001_two_sum/solve1/solve_test.go
@@ -0,0 +1,27 @@
+package solve1
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_two_sum(t *testing.T) {
+	testTable := []struct {
+		name   string
+		nums   []int
+		target int
+	}{
+		{
+			name:   "case 1",
+			nums:   []int{2, 7, 11, 15},
+			target: 9,
+		},
+	}
+
+	for _, c := range testTable {
+		t.Run(c.name, func(t *testing.T) {
+			result := twoSum(c.nums, c.target)
+			assert.Equal(t, c.target, c.nums[result[0]]+c.nums[result[1]])
+		})
+	}
+}
