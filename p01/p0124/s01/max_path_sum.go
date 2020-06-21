eb14c7854944e219e48d6e8561bf9c15f47645de
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-06-21 17:53:29 +0800

    resolve p0124

---
+++ p01/p0124/s01/max_path_sum.go
@@ -0,0 +1,47 @@
+package s01
+
+import (
+	"math"
+
+	"github.com/sui-zhiyuan/leetcode_go/define"
+)
+
+// TreeNode ...
+type TreeNode = define.TreeNode
+
+func maxPathSum(root *TreeNode) int {
+	_, maxDouble := caculate(root)
+	return maxDouble
+}
+
+func caculate(root *TreeNode) (maxSingle, maxDouble int) {
+	if root.Left == nil && root.Right == nil {
+		return max(0, root.Val), root.Val
+	}
+
+	if root.Right == nil {
+		maxLeftSingle, maxLeftDouble := caculate(root.Left)
+		return max(0, root.Val+maxLeftSingle), max(root.Val+maxLeftSingle, maxLeftDouble)
+	}
+
+	if root.Left == nil {
+		maxRightSingle, maxRightDouble := caculate(root.Right)
+		return max(0, root.Val+maxRightSingle), max(root.Val+maxRightSingle, maxRightDouble)
+	}
+
+	maxLeftSingle, maxLeftDouble := caculate(root.Left)
+	maxRightSingle, maxRightDouble := caculate(root.Right)
+	maxSingle = max(0, root.Val+maxLeftSingle, root.Val+maxRightSingle)
+	maxDouble = max(maxLeftDouble, maxRightDouble, root.Val+maxLeftSingle+maxRightSingle)
+	return maxSingle, maxDouble
+}
+
+func max(s ...int) int {
+	r := math.MinInt32
+	for _, v := range s {
+		if r < v {
+			r = v
+		}
+	}
+	return r
+}
---
+++ p01/p0124/s01/max_path_sum_test.go
@@ -0,0 +1,65 @@
+package s01
+
+import (
+	"testing"
+
+	"github.com/sui-zhiyuan/leetcode_go/define"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMaxPathSum(t *testing.T) {
+	testTable := []struct {
+		name string
+		tree *TreeNode
+		path int
+	}{
+		{
+			name: "case 1",
+			tree: define.NewTree([]int{1, 2, 3}),
+			path: 6,
+		},
+		{
+			name: "case 2",
+			tree: define.NewTree([]int{-10, 9, 20, define.Null, define.Null, 15, 7}),
+			path: 42,
+		},
+		{
+			name: "case 3",
+			tree: define.NewTree([]int{-3}),
+			path: -3,
+		},
+		{
+			name: "case 4",
+			tree: define.NewTree([]int{2, -1}),
+			path: 2,
+		},
+		{
+			name: "case 5",
+			tree: define.NewTree([]int{-1, -2, -3}),
+			path: -1,
+		},
+		{
+			name: "case 6",
+			tree: define.NewTree([]int{1, -2, 3}),
+			path: 4,
+		},
+		{
+			name: "case 7",
+			tree: define.NewTree([]int{5, 4, 8, 11, define.Null, 13, 4, 7, 2, define.Null, define.Null, define.Null, define.Null, define.Null, 1}),
+			path: 48,
+		},
+		{
+			name: "case 8",
+			tree: define.NewTree([]int{9, -2, define.Null, -3}),
+			path: 9,
+		},
+	}
+
+	for _, c := range testTable {
+		t.Run(c.name, func(t *testing.T) {
+			path := maxPathSum(c.tree)
+			assert.Equal(t, c.path, path)
+		})
+	}
+}
