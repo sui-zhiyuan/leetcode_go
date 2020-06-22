032c71a4fcc42048f6167ec8bbe02f8f10ccfbd4
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-06-22 11:09:12 +0800

    resolve p1267

---
+++ p12/p1267/s01/count_servers.go
@@ -0,0 +1,22 @@
+package s01
+
+func countServers(grid [][]int) int {
+	row, col := make([]int, len(grid)), make([]int, len(grid[0]))
+	for i, gridRow := range grid {
+		for j, v := range gridRow {
+			if v == 1 {
+				row[i]++
+				col[j]++
+			}
+		}
+	}
+	count := 0
+	for i, gridRow := range grid {
+		for j, v := range gridRow {
+			if v == 1 && (row[i] >= 2 || col[j] >= 2) {
+				count++
+			}
+		}
+	}
+	return count
+}
