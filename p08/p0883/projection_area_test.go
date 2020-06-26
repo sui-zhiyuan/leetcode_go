bd0051373b9f143766e86ea58ee939a0bfae185d
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-06-26 22:02:18 +0800

    resolve p0883

---
+++ p08/p0883/projection_area_test.go
@@ -0,0 +1,33 @@
+package p0883
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProjectionArea(t *testing.T) {
+	testTable := []struct {
+		name string
+		grid [][]int
+		area int
+	}{
+		{
+			name: "case 1",
+			grid: [][]int{{2}},
+			area: 5,
+		},
+		{
+			name: "case 2",
+			grid: [][]int{{1, 2}, {3, 4}},
+			area: 17,
+		},
+	}
+
+	for _, c := range testTable {
+		t.Run(c.name, func(t *testing.T) {
+			area := projectionArea(c.grid)
+			assert.Equal(t, c.area, area)
+		})
+	}
+}
