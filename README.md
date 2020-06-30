cbc6b9a270ccee08a8b022963de6609fdef45606
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-06-30 18:39:17 +0800

    resolve po09

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 p00-p15 std problems
 
-p_interview from 程序员面试金典 problem starts with 面试题
+p_interview from `程序员面试金典` problem starts with `面试题`
+
+p_offer from `剑指 Offer` problem starts with `剑指 Offer`
---
+++ p_offer/po09/cqueue_test.go
@@ -0,0 +1,33 @@
+package po09
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCQueue(t *testing.T) {
+	testTable := []struct {
+		name     string
+		elements []int
+	}{
+		{
+			name:     "case 1",
+			elements: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+	}
+
+	for _, c := range testTable {
+		t.Run(c.name, func(t *testing.T) {
+			queue := Constructor()
+			for _, v := range c.elements {
+				queue.AppendTail(v)
+			}
+			elements := []int{}
+			for v := queue.DeleteHead(); v != -1; v = queue.DeleteHead() {
+				elements = append(elements, v)
+			}
+			assert.Equal(t, c.elements, elements)
+		})
+	}
+}
