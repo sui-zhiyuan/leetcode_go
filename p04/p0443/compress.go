978bc0bbad62baf8bdb99b3e7240a2588d6e2341
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-07-01 11:14:39 +0800

    resolve p0443

---
+++ p04/p0443/compress.go
@@ -0,0 +1,39 @@
+package p0443
+
+import (
+	"fmt"
+)
+
+func compress(chars []byte) int {
+	currChar := byte('-')
+	currCount := 0
+	writePos := 0
+	for _, v := range chars {
+		if v == currChar {
+			currCount++
+			continue
+		}
+		writePos = write(chars, writePos, currChar, currCount)
+
+		currChar = v
+		currCount = 1
+	}
+	writePos = write(chars, writePos, currChar, currCount)
+	return writePos
+}
+
+func write(chars []byte, writePos int, char byte, count int) (writePosOut int) {
+	if count == 0 {
+		return writePos
+	}
+	chars[writePos] = char
+	writePos++
+	if count > 1 {
+		str := fmt.Sprint(count)
+		for i := 0; i < len(str); i++ {
+			chars[writePos] = str[i]
+			writePos++
+		}
+	}
+	return writePos
+}
