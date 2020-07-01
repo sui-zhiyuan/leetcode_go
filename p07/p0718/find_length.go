29c9799a69c8397ffccb606e58b962d9b6db736f
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-07-01 10:43:48 +0800

    resolve p0781

---
+++ p07/p0718/find_length.go
@@ -0,0 +1,33 @@
+package p0718
+
+func findLength(A []int, B []int) int {
+	prev := make([]int, len(A))
+	curr := make([]int, len(A))
+
+	ans := 0
+	for _, vB := range B {
+		for i := range prev {
+			prev[i] = 0
+		}
+		curr, prev = prev, curr
+
+		for i, vA := range A {
+			p1 := 0
+			if i > 0 {
+				p1 = prev[i-1]
+			}
+			if vA == vB {
+				curr[i] = p1 + 1
+				if curr[i] > ans {
+					ans = curr[i]
+				}
+			}
+
+			//fmt.Println("step", p1, p2, p3, vA, vB)
+		}
+
+		//fmt.Println("roud", vB, curr)
+	}
+
+	return ans
+}
---
+++ p07/p0718/find_length_test.go
@@ -0,0 +1,36 @@
+package p0718
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFindLength(t *testing.T) {
+	testTable := []struct {
+		name   string
+		A      []int
+		B      []int
+		length int
+	}{
+		{
+			name:   "case 1",
+			A:      []int{1, 2, 3, 2, 1},
+			B:      []int{3, 2, 1, 4, 7},
+			length: 3,
+		},
+		{
+			name:   "case 2",
+			A:      []int{0, 1, 1, 1, 1},
+			B:      []int{1, 0, 1, 0, 1},
+			length: 2,
+		},
+	}
+
+	for _, c := range testTable {
+		t.Run(c.name, func(t *testing.T) {
+			length := findLength(c.A, c.B)
+			assert.Equal(t, c.length, length)
+		})
+	}
+}
