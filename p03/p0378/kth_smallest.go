0ff311140bd6e41ea0240e9b370c8f52e1b3f132
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-07-02 21:51:17 +0800

    resolve p0378

---
+++ p03/p0378/kth_smallest.go
@@ -0,0 +1,26 @@
+package p0378
+
+func kthSmallest(matrix [][]int, k int) int {
+	n := len(matrix)
+	arr := make([]int, 0, n*n)
+	curr := make([]int, n)
+
+	for {
+		next := -1
+		min := 0
+		for i := range matrix {
+			if curr[i] < n && (next < 0 || matrix[i][curr[i]] < min) {
+				next = i
+				min = matrix[i][curr[i]]
+			}
+		}
+
+		if next == -1 {
+			break
+		}
+
+		arr = append(arr, min)
+		curr[next]++
+	}
+	return arr[k-1]
+}
