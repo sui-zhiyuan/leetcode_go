fc96b9e2fb9df32d1689593481eca836a94d28af
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-07-06 17:31:08 +0800

    resolve week race 196

---
+++ p54/p5453/get_last_moment.go
@@ -0,0 +1,18 @@
+package p5453
+
+func getLastMoment(n int, left []int, right []int) int {
+	ans := 0
+	for _, v := range left {
+		if v > ans {
+			ans = v
+		}
+	}
+
+	for _, v := range right {
+		if n-v > ans {
+			ans = n - v
+		}
+	}
+
+	return ans
+}
---
+++ p54/p5454/num_submat.go
@@ -0,0 +1,39 @@
+package p5454
+
+func numSubmat(mat [][]int) int {
+	lenRow := len(mat)
+	lenCol := len(mat[0])
+	ans := 0
+	for row, colV := range mat {
+		for col, v := range colV {
+			if v == 1 {
+				ans += countSqure(mat, lenRow, lenCol, row, col)
+			}
+
+		}
+	}
+	return ans
+}
+
+func countSqure(mat [][]int, lenRow, lenCol int, row, col int) int {
+	result := 0
+	maxCol := lenCol
+	for rows := row; rows < lenRow; rows++ {
+		cols := col
+		for ; cols < maxCol; cols++ {
+			if mat[rows][cols] == 1 {
+				result++
+				continue
+			}
+			if maxCol >= cols {
+				maxCol = cols
+			}
+			break
+		}
+		if cols == col {
+			break
+		}
+	}
+
+	return result
+}
