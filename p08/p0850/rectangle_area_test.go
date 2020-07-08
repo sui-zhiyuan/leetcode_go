7c103fcee22ec715a6289715e4c9daea859f8296
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-07-08 11:51:48 +0800

    resolve p0850

---
+++ p08/p0850/rectangle_area_test.go
@@ -0,0 +1,42 @@
+package p0850
+
+import "testing"
+
+func Test_rectangleArea(t *testing.T) {
+	type args struct {
+		rectangles [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				rectangles: [][]int{
+					{0, 0, 2, 2},
+					{1, 0, 2, 3},
+					{1, 0, 3, 1},
+				},
+			},
+			want: 6,
+		},
+		{
+			name: "case 2",
+			args: args{
+				rectangles: [][]int{
+					{0, 0, 1000000000, 1000000000},
+				},
+			},
+			want: 49,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := rectangleArea(tt.args.rectangles); got != tt.want {
+				t.Errorf("rectangleArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
