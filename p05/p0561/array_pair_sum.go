e8955cc2a5324ea62897254d02ef4808e2cd3022
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-07-08 12:02:21 +0800

    resolve p0561

---
+++ p05/p0561/array_pair_sum.go
@@ -0,0 +1,16 @@
+package p0561
+
+import (
+	"sort"
+)
+
+func arrayPairSum(nums []int) int {
+	n := len(nums) / 2
+	sort.Slice(nums, func(i, j int) bool { return nums[i] < nums[j] })
+
+	result := 0
+	for i := 0; i < n; i++ {
+		result += nums[i*2]
+	}
+	return result
+}
