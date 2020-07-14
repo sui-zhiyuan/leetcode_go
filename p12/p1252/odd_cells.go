59051ec16ec5ddb5c7b0a78f2581a9fbd85ba4ee
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-07-14 12:42:02 +0800

    resolve p1252

---
+++ p12/p1252/odd_cells.go
@@ -0,0 +1,22 @@
+package p1252
+
+func oddCells(n int, m int, indices [][]int) int {
+	rows := make([]bool, n)
+	cols := make([]bool, m)
+
+	for _, v := range indices {
+		rows[v[0]] = !rows[v[0]]
+		cols[v[1]] = !cols[v[1]]
+	}
+
+	result := 0
+
+	for row := 0; row < n; row++ {
+		for col := 0; col < m; col++ {
+			if rows[row] && !cols[col] || !rows[row] && cols[col] {
+				result++
+			}
+		}
+	}
+	return result
+}
---
+++ p12/p1252/odd_cells_test.go
@@ -0,0 +1,42 @@
+package p1252
+
+import "testing"
+
+func Test_oddCells(t *testing.T) {
+	type args struct {
+		n       int
+		m       int
+		indices [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				n:       2,
+				m:       3,
+				indices: [][]int{{0, 1}, {1, 1}},
+			},
+			want: 6,
+		},
+		{
+			name: "case 2",
+			args: args{
+				n:       2,
+				m:       2,
+				indices: [][]int{{1, 1}, {0, 0}},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := oddCells(tt.args.n, tt.args.m, tt.args.indices); got != tt.want {
+				t.Errorf("oddCells() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
