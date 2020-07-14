e9e143d35abcb8811d05de849e521cf72f4bd78d
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-07-14 21:46:23 +0800

    resolve p1163

---
+++ p11/p1163/last_sub_string_test.go
@@ -0,0 +1,42 @@
+package p1163
+
+import "testing"
+
+func Test_lastSubstring(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "case 1",
+			args: args{s: "abab"},
+			want: "bab",
+		},
+		{
+			name: "case 2",
+			args: args{s: "leetcode"},
+			want: "tcode",
+		},
+		{
+			name: "case 3",
+			args: args{s: "xbylisvborylklftlkcioajuxwdhahdgezvyjbgaznzayfwsaumeccpfwamfzmkinezzwobllyxktqeibfoupcpptncggrdqbkji"},
+			want: "zzwobllyxktqeibfoupcpptncggrdqbkji",
+		},
+		{
+			name: "case 4",
+			args: args{s: "cacacb"},
+			want: "cb",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lastSubstring(tt.args.s); got != tt.want {
+				t.Errorf("lastSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
