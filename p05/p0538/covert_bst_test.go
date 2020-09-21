48611ef169d9aeca85b4551abde4bdd54db5ec18
Zhiyuan Sui <suizhiyuan021435@gmail.com>
2020-09-21 10:50:31 +0800

    resolve p0538

---
+++ p05/p0538/covert_bst_test.go
@@ -0,0 +1,34 @@
+package p0538
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/sui-zhiyuan/leetcode_go/define"
+)
+
+func Test_convertBST(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{
+			name: "case 1",
+			args: args{
+				root: define.NewTree([]int{5, 2, 13}),
+			},
+			want: define.NewTree([]int{18, 20, 13}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convertBST(tt.args.root); !cmp.Equal(got, tt.want) {
+				t.Errorf("convertBST() = %v, want %v\ndiff=%v", got, tt.want, cmp.Diff(got, tt.want))
+			}
+		})
+	}
+}
