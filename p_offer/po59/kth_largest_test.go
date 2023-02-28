fb66f0a620f9e53843a996c952968730c28343b3
Sui Zhiyuan <suizhiyuan021435@gmail.com>
2023-02-28 11:22:17 +0800

    resolve po59

---
+++ p_offer/po59/kth_largest_test.go
@@ -0,0 +1,37 @@
+package po59
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd(t *testing.T) {
+	type inner struct {
+		input  int
+		except int
+	}
+
+	cases := []struct {
+		name string
+		k    int
+		nums []int
+		val  []inner
+	}{
+		{
+			name: "case 1",
+			k:    3,
+			nums: []int{4, 5, 8, 2},
+			val:  []inner{{3, 4}, {5, 5}, {10, 5}, {9, 8}, {4, 8}},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			kl := Constructor(c.k, c.nums)
+			for _, v := range c.val {
+				assert.Equal(t, v.except, kl.Add(v.input))
+			}
+		})
+	}
+}
