b12616a9b8e464d8334af2f7eb674812b127e7c3
Sui Zhiyuan <suizhiyuan021435@gmail.com>
2025-07-31 09:35:31 +0800

    resolve p2683

---
+++ rust/src/l26/l2683.rs
@@ -0,0 +1,8 @@
+pub fn does_valid_array_exist(derived: Vec<i32>) -> bool {
+    // assume first value is 0
+    let mut value = 0;
+    for v in derived {
+        value = value ^ v;
+    }
+    value == 0
+}
--- rust/src/l26/mod.rs
+++ rust/src/l26/mod.rs
@@ -1,3 +1,4 @@
 pub mod l2639;
 pub mod l2644;
 pub mod l2663;
+pub mod l2683;
