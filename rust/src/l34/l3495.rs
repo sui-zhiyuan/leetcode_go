594f2206ef241b773e39b460d9a824f8361e15d8
Sui Zhiyuan <suizhiyuan021435@gmail.com>
2025-09-06 09:13:41 +0800

    resolve l3495

---
+++ rust/src/l34/l3495.rs
@@ -0,0 +1,19 @@
+pub fn min_operations(queries: Vec<Vec<i32>>) -> i64 {
+    let mut ans = 0;
+
+    for q in queries {
+        ans += (sum_to_n(q[1]) - sum_to_n(q[0] - 1) + 1) / 2;
+    }
+
+    ans
+}
+
+fn sum_to_n(n: i32) -> i64 {
+    let m = (size_of_val(&n) * 8) as i64 - n.leading_zeros() as i64;
+    let mut ret = 0;
+    for i in 1..m {
+        ret += (i + 1) / 2 << (i - 1);
+    }
+
+    ret + (m + 1) / 2 * (n as i64 + 1 - (1 << (m as usize - 1)))
+}
--- rust/src/l34/mod.rs
+++ rust/src/l34/mod.rs
@@ -2,3 +2,4 @@ pub mod l3459;
 pub mod l3446;
 pub mod l3477;
 pub mod l3487;
+pub mod l3495;
