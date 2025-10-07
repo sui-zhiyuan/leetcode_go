27f675c5f6a768077cd6acf1f01bde5c7bbddfc9
Sui Zhiyuan <suizhiyuan021435@gmail.com>
2025-10-07 15:43:59 +0800

    refactor commit script

--- util/commit.py
+++ util/commit.py
@@ -1,63 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import io
 import subprocess
 import sys
 import pathlib
 import os
 import re
-
-curr = os.path.abspath(__file__)
-root = pathlib.Path(curr).parent.parent.resolve()
-
-file = sys.argv[1]
-if not file.startswith("l") or len(file) < 5:
-    raise Exception(
-        "invalid file name, file name should start with l and followed by a 4 digit number"
-    )
-
-file = pathlib.Path(root, "rust", "src", file[0:3], file + ".rs")
-if not file.exists():
-    raise Exception("file does not exist", file)
-
-text = file.read_text()
-
-placeholder = [
-    "type TreeNode = crate::common::TreeNode<[fiu]([0-9]+|size)>;\n",
-    "type ListNode = crate::common::ListNode<[fiu]([0-9]+|size)>;\n",
-]
-for r in placeholder:
-    m = re.search(r, text)
-    if m:
-        text = text.replace(m.group(0), "")
-
-# remove debug and test codes
-solution_index = text.find("\n// debug and test")
-if solution_index < 0:
-    solution_index = text.find("\nimpl Debug for")
-if solution_index < 0:
-    solution_index = text.find("\n#[cfg(test)]")
-if solution_index >= 0:
-    text = text[0:solution_index]
-
-first_fn_start = text.find("pub fn ")
-if first_fn_start < 0:
-    raise Exception("no function found")
-first_fn_end = text.find("\n}", first_fn_start)
-if first_fn_end < 0:
-    raise Exception("no function end found")
-first_fn_end += 2
-prefix = text[0:first_fn_start]
-middle = text[first_fn_start:first_fn_end]
-suffix = text[first_fn_end:]
-text = (
-    prefix
-    + "impl Solution {\n"
-    + "".join(["    " + v for v in middle.splitlines(True)])
-    + "\n}"
-    + suffix
-)
 
 
 class Replacement:
@@ -68,6 +16,11 @@ class Replacement:
         self.placeholder = placeholder
         self.file_name = file_name
 
+
+placeholders = [
+    "type TreeNode = crate::common::TreeNode<[fiu]([0-9]+|size)>;\n",
+    "type ListNode = crate::common::ListNode<[fiu]([0-9]+|size)>;\n",
+]
 
 replacements = [
     Replacement("use crate::common::SegmentTree;", "segment_tree.rs"),
@@ -80,45 +33,116 @@ replacements = [
     Replacement("use crate::common::binary_search;", "binary_search.rs"),
 ]
 
-for r in replacements:
-    seg_index = text.find(r.placeholder)
-    if seg_index < 0:
-        continue
+root = None
 
-    # replace use
-    mod_name = r.file_name.removesuffix(".rs")
-    struct_name = r.placeholder.removeprefix("use crate::common::").removesuffix(";")
-    replace_use = "use {mod}::{struct};".format(mod=mod_name, struct=struct_name)
-    text = text.replace(r.placeholder, replace_use)
-    text += "\n"
 
-    # add text to end
-    seg_path = pathlib.Path(root, "rust", "src", "common", r.file_name)
-    seg_text = seg_path.read_text()
-    seg_index = seg_text.find("// debug and test")
-    if seg_index >= 0:
-        seg_text = seg_text[0:seg_index]
-    while seg_text.startswith("\n"):
-        seg_text = seg_text.removeprefix("\n")
-    while seg_text.endswith("\n"):
-        seg_text = seg_text.removesuffix("\n")
+def get_project_root() -> pathlib.Path:
+    global root
+    if root is not None:
+        return root
+    curr = os.path.abspath(__file__)
+    root = pathlib.Path(curr).parent.parent.resolve()
+    return root
 
-    seg_text = (
-        "mod {mod}{{\n".format(mod=mod_name)
-        + "".join(["    " + v for v in seg_text.splitlines(True)])
-        + "}\n"
+
+def normal_input_path() -> pathlib.Path:
+    if len(sys.argv) != 2:
+        current_file = os.path.basename(__file__)
+        message = f"invalid parameter, use: {current_file} <target_file>"
+        raise Exception(message)
+    file = sys.argv[1]
+    if not file.startswith("l") or len(file) < 5:
+        raise Exception(
+            "invalid file name, file name should start with l and followed by a 4 digit number"
+        )
+
+    file = pathlib.Path(get_project_root(), "rust", "src", file[0:3], file + ".rs")
+    if not file.exists():
+        raise Exception("file does not exist", file)
+    return file
+
+
+def remove_debug_code(text: str) -> str:
+    solution_index = text.find("\n// debug and test")
+    if solution_index < 0:
+        solution_index = text.find("\nimpl Debug for")
+    if solution_index < 0:
+        solution_index = text.find("\n#[cfg(test)]")
+    if solution_index >= 0:
+        text = text[0:solution_index]
+    return text
+
+
+def process_code(text: str) -> str:
+    for r in placeholders:
+        m = re.search(r, text)
+        if m:
+            text = text.replace(m.group(0), "")
+    # remove debug and test codes
+    text = remove_debug_code(text)
+    first_fn_start = text.find("pub fn ")
+    if first_fn_start < 0:
+        raise Exception("no function found")
+    first_fn_end = text.find("\n}", first_fn_start)
+    if first_fn_end < 0:
+        raise Exception("no function end found")
+    first_fn_end += 2
+    prefix = text[0:first_fn_start]
+    middle = text[first_fn_start:first_fn_end]
+    suffix = text[first_fn_end:]
+    text = (
+            prefix
+            + "impl Solution {\n"
+            + "".join(["    " + v for v in middle.splitlines(True)])
+            + "\n}"
+            + suffix
     )
+    for r in replacements:
+        seg_index = text.find(r.placeholder)
+        if seg_index < 0:
+            continue
 
-    text += seg_text
+        # replace use
+        mod_name = r.file_name.removesuffix(".rs")
+        struct_name = r.placeholder.removeprefix("use crate::common::").removesuffix(";")
+        replace_use = "use {mod}::{struct};".format(mod=mod_name, struct=struct_name)
+        text = text.replace(r.placeholder, replace_use)
+        text += "\n"
 
-    
+        # add text to end
+        seg_path = pathlib.Path(get_project_root(), "rust", "src", "common", r.file_name)
+        seg_text = seg_path.read_text()
+        seg_text = remove_debug_code(seg_text)
+        while seg_text.startswith("\n"):
+            seg_text = seg_text.removeprefix("\n")
+        while seg_text.endswith("\n"):
+            seg_text = seg_text.removesuffix("\n")
 
-text_input = io.StringIO(text)
-process = subprocess.Popen(
-    ["code", "-"],
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    shell=True,
-)
-process.communicate(text.encode())
+        seg_text = (
+                "mod {mod}{{\n".format(mod=mod_name)
+                + "".join(["    " + v for v in seg_text.splitlines(True)])
+                + "\n}\n"
+        )
+        text += seg_text
+    return text
+
+
+def show_in_vscode(text: str):
+    process = subprocess.Popen(
+        ["code", "-"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    process.communicate(text.encode())
+
+
+def main():
+    text = normal_input_path().read_text()
+    text = process_code(text)
+    show_in_vscode(text)
+
+
+if __name__ == "__main__":
+    main()
