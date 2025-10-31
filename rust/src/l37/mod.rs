4e66604806beb76c41d992681dca78b11a1e9f74
Sui Zhiyuan <suizhiyuan021435@gmail.com>
2025-10-31 08:36:10 +0800

    cleanup double week race 168

--- rust/src/l37/mod.rs
+++ rust/src/l37/mod.rs
@@ -2,3 +2,7 @@ pub mod l3712;
 pub mod l3713;
 pub mod l3714;
 pub mod l3715;
+pub mod l3722;
+pub mod l3723;
+pub mod l3724;
+pub mod l3725;
--- rust/src/l96/mod.rs
+++ rust/src/l96/mod.rs
@@ -1,7 +1,3 @@
-pub mod l9605;
-pub mod l9606;
-pub mod l9607;
-pub mod l9608;
 pub mod l9611;
 pub mod l9612;
 pub mod l9613;
